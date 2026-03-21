d4796a67fbdcd63386025c53e46042922ca87a74
sanjay ponaganti <24eg105m58@anurag.edu.in>
2026-03-21 10:50:50 +0530

    Update validator.js.
    Signed-off-by: sanjay ponaganti <24eg105m58@anurag.edu.in>

--- week-2/day-3/modules_01/validator.js
+++ week-2/day-3/modules_01/validator.js
@@ -1,23 +1,24 @@
-function validateTitle(title){
-  if(!title){
-    return "Title Required"
+function validateTitle(title) {
+  if (!title) {
+    return "Title Required";
   }
-  if(title.length<3){
-    return "Min 3 characters are required"
+  if (title.length < 3) {
+    return "Min 3 characters are required";
   }
-  return title
+  return true;
 }
-function validatePriority(priority){
-  if(priority=="LOW" || priority=="MID" || priority=="HIGH"){
-    return priority
+function validatePriority(priority) {
+  if (priority == "LOW" || priority == "MID" || priority == "HIGH") {
+    return true;
   }
+  return "priority must be LOW,MID,HIGH";
 }
-function validateDueDate(date){
-  let dueDate=new '2026-01-02'
-  let today=new date()//for system date
-  if(dueDate<today){
-    return "Expired"
+function validateDueDate(dateStr) {
+  let dueDate = new Date(dateStr);
+  let today = new Date(); //for system date
+  if (dueDate < today) {
+    return "Expired";
   }
-  return dueDate
+  return true;
 }
-export {validateTitle, validatePriority, validateDueDate}
+export { validateTitle, validatePriority, validateDueDate };
