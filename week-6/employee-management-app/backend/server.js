5cf043718fd104ba8ad2d6909b65d036c756c8d3
24eg105m58-a11y <24eg105m58@anurag.edu.in>
2026-05-16 18:05:47 +0530

    added start script

--- week-6/employee-management-app/backend/server.js
+++ week-6/employee-management-app/backend/server.js
@@ -1,23 +1,47 @@
 import exp from "express";
-import { employeeModel } from "./models/employeeModel.js";
 import { employeeApp } from "./Api's/employeeApi.js";
 import { connect } from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = exp();
 
-//add cors middleware
-app.use(cors({ origin: ["http://localhost:5175"] }));
+// CORS
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-frontend.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
+// middleware
 app.use(exp.json());
+
+// routes
 app.use("/employee-api", employeeApp);
-const port = 5432;
+
+// PORT
+const port = process.env.PORT || 4000;
+
+// DB Connection
 async function connectDb() {
   try {
-    await connect("mongodb://localhost:27017/EmployeeDemo");
-    console.log("databasse connection succesfull");
-    app.listen(port, () => console.log(`server is listening on ${port}`));
+    await connect(process.env.DB_URL);
+
+    console.log("database connection successful");
+
+    app.listen(port, () =>
+      console.log(`server is listening on ${port}`)
+    );
   } catch (err) {
-    console.log("database connection falied");
+    console.log("database connection failed");
+    console.log(err);
   }
 }
+
 connectDb();
